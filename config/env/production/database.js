c05786333e4bc694ff649bd03a660dabbaabbddf
jorgexVP <89286580+jorgexVP@users.noreply.github.com>
2023-05-07 16:44:20 -0500

    Fix(PRUEB)

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,18 @@
-import { parse } from 'pg-connection-string';
-const config = parse(process.env.DATABASE_URL);
 
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+
+    export default ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'), 
+          port: env.int('DATABASE_PORT'), 
+          database: env('DATABASE_NAME'), 
+          user: env('DATABASE_USERNAME'), 
+          password: env('DATABASE_PASSWORD'),
+          ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
       },
-    },
-    debug: false,
-  },
-});
+    });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 // Path: ./config/env/production/server.js`
 export default ({ env }) => ({
         proxy: true,
-        url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+        url: env('APP_URL'), // Sets the public URL of the application.
         app: { 
           keys: env.array('APP_KEYS')
         },
